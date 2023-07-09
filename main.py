5bfdf9065b09df5284794514c75266046075b0b4
Shawn Lim <shawn.lim@local>
2023-07-09 21:50:29 +0800

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+import json
+
+def hello(event, context):
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json",
+        },
+        "body": json.dumps({"status": "ok"}),
+    }
+
